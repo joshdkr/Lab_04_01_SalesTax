dec82e4e1d2c0e1d252fb92afc200f9c94e5a02b
joshuakrempasky <joshkrempasky@gmail.com>
2022-09-18 22:55:51 -0400

    innit push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double purchasePrice = 25.50;
+        double salesTax = 0.05;
+
+        System.out.println("The total price of an item costing $" + purchasePrice + " with " + salesTax + " sales tax is $" + ((purchasePrice * salesTax) + purchasePrice));
     }
 }
